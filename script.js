0848798fed858e61ac70e25322e2efc723d0c87a
Saksham <atomixdes@icloud.com>
2024-05-28 18:13:32 -0400

    Add working functions for buttons.

--- script.js
+++ script.js
@@ -21,17 +21,13 @@ function operate(num1, operator, num2) {
 
     switch (operator) {
         case "+":
-            add(num1, num2);
-            break;
+            return add(num1, num2);
         case "-":
-            subtract(num1, num2);
-            break;
+            return subtract(num1, num2);
         case "*":
-            multiply(num1, num2);
-            break;
+            return multiply(num1, num2);
         case "/":
-            divide(num1, num2);
-            break;
+            return divide(num1, num2);
         default:
             return "Error: Unknown Operator";
     }
@@ -39,5 +35,70 @@ function operate(num1, operator, num2) {
 }
 
 let operator = "";
-let num1 = 0;
-let num2 = 0;
+let num1 = null;
+let num2 = null;
+
+const screenText = document.querySelector(".screen-text");
+
+const numberBut = document.querySelectorAll(".number-container button");
+numberBut.forEach(button => {
+    button.addEventListener("click", () => {
+        if (!(screenText.textContent.length > 15)) {
+            if (button.textContent === ".") {
+                if (!(screenText.textContent.includes("."))) {
+                    screenText.textContent += button.textContent;
+                }
+            }
+            else {
+                if (screenText.textContent === "0" && operator === "") {
+                    screenText.textContent = button.textContent;
+                }
+                else {
+                    screenText.textContent += button.textContent;
+                }
+            }
+        }
+    });
+    
+});
+
+const clearBut = document.querySelector("#b-Clear");
+clearBut.addEventListener("click", () => {
+    screenText.textContent = "0";
+    num1 = null;
+    num2 = null;
+    operator = "";
+});
+
+const operationBut = document.querySelectorAll(".operation-container button");
+operationBut.forEach(button => {
+    button.addEventListener("click", () => {
+        if (button.textContent === "=") {
+            if (num1 != null && operator != "" && screenText.textContent != "") {
+                num2 = Number(screenText.textContent);
+                screenText.textContent = operate(num1, operator, num2);
+                num1 = Number(screenText.textContent);
+                operator = "";
+            }
+        }
+        else {
+            if (num1 == null) {
+                num1 = Number(screenText.textContent);
+                operator = button.textContent;
+                screenText.textContent = "";
+            }
+            else if (num1 != null && operator != "" && screenText.textContent != "") {
+                num2 = Number(screenText.textContent);
+                screenText.textContent = operate(num1, operator, num2);
+                num1 = Number(screenText.textContent);
+                operator = button.textContent;
+
+            }
+            else {
+                operator = button.textContent;
+                screenText.textContent = "";
+            }
+        }
+        
+    });
+});
